b772b251368b2be401e46407eaea1cc587413432
Dlove <dlove327@gmail.com>
2025-02-17 15:19:56 -0600

    Syntax excercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Threading.Channels;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var answer = 10;
+           var response = answer < 9 ? $"{answer} is less that nine" : $"{answer} is greater than or equal to nine";
+           Console.WriteLine(response);
         }
     }
 }
